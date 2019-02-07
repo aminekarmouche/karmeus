b2998604e12817a6e65774285234e4590d818cb6
Amine Karmouche <amka@netlight.com>
2019-02-07 10:55:38 +0100

    alternative approach

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,17 +4,17 @@
 set -e
 
 # Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-echo "$PRIVATE_KEY"
-echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+#eval $(ssh-agent -s)
+#echo "$PRIVATE_KEY"
+#echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 # ** Alternative approach
-# echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
-# chmod 600 /root/.ssh/id_rsa
+echo -e "$PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 # ** End of alternative approach
 
 # disable the host key checking.
-#chmod u+x ./deploy/disableHostKeyChecking.sh
+chmod u+x ./deploy/disableHostKeyChecking.sh
 
 # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
 # comma seperated values of ip addresses.
